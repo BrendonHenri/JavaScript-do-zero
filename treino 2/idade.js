e753e3faf4ffea2f1254f4cac026de2cc439565b
Brendon <128165420+BrendonHenri@users.noreply.github.com>
2023-04-16 21:34:16 +0200

    começando bem

---
+++ treino 2/idade.js
@@ -0,0 +1,27 @@
+
+
+function iniciar(){
+    const dataNascimento = new Date(document.getElementById('idata').value);
+    const sexo = document.querySelector('input[name="sexo"]:checked').value;
+    const anoAtual = new Date ().getFullYear();
+    const anoDeNascimento = dataNascimento.getFullYear();
+    const idade = anoAtual - anoDeNascimento;
+    const inputs = document.querySelectorAll('input[type="radio"], input[type="date"]')
+    const labels = document.querySelectorAll('label[for^="i"]')
+    let pessoa = document.getElementById ('image')
+    const botao = document.getElementById ('btn-vai')
+    const resposta = document.createElement('p')
+    const card = document.getElementById ('card')
+    if (sexo === 'masc'){
+        inputs.forEach(function(input) {
+        input.style.display = 'none'});
+        labels.forEach(function(label){
+            label.style.display = 'none'
+        })
+        botao.style.display = 'none'
+        pessoa.style.backgroundImage = 'url("imagens/homen-joven.jpg")'
+        resposta.innerHTML= `Você tem ${idade} anos e é um um homen.`
+        resposta.style.fontSize = "2em";
+        card.appendChild(resposta)
+    }
+}
